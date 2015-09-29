7075496871d98bbbd416cee8147aa929cc44e4f5
Darshan Parajuli <parajulidarshan@gmail.com>
2015-09-28 19:55:01 -0700

    search by name works

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from pathlib import Path
 
-def search_by_name(root, file_name):
-    print('search by name')
+def search_by_name(root:str, file_name:str, result):
+    files = Path(root).iterdir()
+    for f in files:
+        if (f.is_dir()):
+            search_by_name(f.resolve(), file_name, result)
+        else:
+            if (f.name == file_name):
+                result.append(f)
+                
 
-#FLAGS = {'N': search_by_name}
-
-
-#def N_Function(
-
-def function_NES(first_letter:str, last_part:str):
+def function_NES(root:str, first_letter:str, last_part:str):
     if first_letter == 'N':
-        print('You typed N')
+        result = []
+        search_by_name(root, last_part, result)
+        print(result)
 
 def main():
-    while 1:
+    root = input()
+    while not Path(root).is_dir():
+        print("ERROR")
         root = input()
-        while not Path(root).is_dir():
-            print("ERROR")
-            root = input()
 
-        flag = input()
-
-        first_letter = flag[0]
-        last_part = flag[2:]
-        print(first_letter, last_part)
-        function_NES(first_letter, last_part)
+    flag = input()
+    
+    first_letter = flag[0]
+    last_part = flag[2:]
+    # print(first_letter, last_part)
+    function_NES(root, first_letter, last_part)
         
 
 if __name__ == '__main__':
