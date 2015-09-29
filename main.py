850472087ef8fe4c9133a4867e944dc7ef1c4c41
Michael Zarour <michaelzarour@dhcp-v026-025.mobile.uci.edu>
2015-09-28 19:35:23 -0700

    Project 1 Update 9-28-15

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from pathlib import Path
 def search_by_name(root, file_name):
     print('search by name')
 
-FLAGS = {'N': search_by_name}
+#FLAGS = {'N': search_by_name}
+
+
+#def N_Function(
+
+def function_NES(first_letter:str, last_part:str):
+    if first_letter == 'N':
+        print('You typed N')
 
 def main():
     while 1:
@@ -12,7 +19,12 @@ def main():
             print("ERROR")
             root = input()
 
-        flag = input().split()
+        flag = input()
+
+        first_letter = flag[0]
+        last_part = flag[2:]
+        print(first_letter, last_part)
+        function_NES(first_letter, last_part)
         
 
 if __name__ == '__main__':
