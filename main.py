e0f0fbc07a224041c8c050c7dd58df035ba5577a
Darshan Parajuli <parajulidarshan@gmail.com>
2015-10-02 18:50:54 -0700

    fixed some stuff, added some stuff

--- main.py
+++ main.py
@@ -1,70 +1,95 @@
 from pathlib import Path
 import os
 
-ACTIONS = {'N': True, 'E': True, 'S': True, 'P': False, 'F': False, 'D': False, 'T': False}
+def verify_search_command(action: str, arg: str) -> bool:
+    search_commands = ['N', 'E', 'S']
 
-search_result = []
-
-def verify_action(action: str, arg: str) -> bool:
     if action == None:
         return False
     
-    available_actions = ACTIONS.keys()
-    if action in available_actions:
-        if ACTIONS[action]:
-            if arg == None or len(arg) == 0:
-                return False
-            else:
-                if action != 'S':
-                    return True
-                else:
-                    try:
-                        int(arg)
-                        return True
-                    except:
-                        return False
+    if action in search_commands:
+        if arg == None or len(arg) == 0:
+            return False
         else:
-            return True
+            if action != 'S':
+                return True
+            else:
+                try:
+                    int(arg)
+                    return True
+                except:
+                    return False
     else:
         return False
 
-def search_by_name(root: str, file_name: str) -> None:
+def verify_action(action: str) -> bool:
+    actions = ['P', 'F', 'D', 'T']
+    return action in actions
+    
+def search_by_name(root: str, file_name: str, search_result: list) -> None:
     files = Path(root).iterdir()
-    for f in files:
-        if (f.is_dir()):
-            search_by_name(str(f.resolve()), file_name)
-        else:
-            if (f.name == file_name):
-                search_result.append(f)
+    try:
+        for f in files:
+            if f.is_dir():
+                search_by_name(str(f), file_name, search_result)
+            elif f.exists():
+                if (f.name == file_name):
+                    search_result.append(f)
+    except PermissionError:
+        print("Access denied: {}".format(root))
                 
-def search_by_ext(root:str, file_ext:str) -> None:
+def search_by_ext(root:str, file_ext:str, search_result: list) -> None:
     files = Path(root).iterdir()
-    for f in files:
-        if (f.is_dir()):
-            search_by_ext(str(f.resolve()), file_ext)
-        else:
-            if (f.suffix == file_ext):
-                search_result.append(f)
+    try:
+        for f in files:
+            if f.is_dir():
+                search_by_ext(str(f.resolve()), file_ext, search_result)
+            elif f.exists():
+                if (f.suffix == file_ext):
+                    search_result.append(f)
+    except PermissionError:
+        print("Access denied: {}".format(root))
 
-
-def search_by_size(root:str, file_size:int) -> None:
+def search_by_size(root:str, file_size:int, search_result: list) -> None:
     files = Path(root).iterdir()
-    for f in files:
-        file_path = str(f.resolve())
-        if (f.is_dir()):
-            search_by_size(file_path, file_size)
-        else:
-            if os.path.getsize(file_path) > file_size:
-                search_result.append(f)
+    try:
+        for f in files:
+            file_path = str(f)
+            if f.is_dir():
+                search_by_size(file_path, file_size, search_result)
+            elif f.exists():
+                if os.path.getsize(file_path) > file_size:
+                    search_result.append(f)
+    except PermissionError:
+        print("Access denied: {}".format(root))
                 
 def handle_search(root:str, action:str, arg:str) -> None:
-    if action == 'N':
-        search_by_name(root, arg)
-    elif action == 'E':
-        search_by_ext(root, arg)
-    elif action == 'S':
-        search_by_size(root, int(arg))
+    search_result = []
     
+    if action == 'N':
+        search_by_name(root, arg, search_result)
+    elif action == 'E':
+        search_by_ext(root, arg, search_result)
+    else: # S
+        search_by_size(root, int(arg), search_result)
+
+    return search_result
+
+def print_file_paths(search_result: list) -> None:
+    for f in search_result:
+        print(str(f))
+
+def handle_actions(action: str, search_result: list) -> None:
+    if action == 'P':
+        print_file_paths(search_result)
+    elif action == 'F':
+        pass
+    elif action == 'D':
+        pass
+    else: # T
+        pass
+    
+
 def main() -> None:
     root = input().strip()
     while not Path(root).is_dir():
@@ -73,24 +98,32 @@ def main() -> None:
 
     second_line = input().strip()
 
-    action = None
+    search_command = None
     arg = None
     if len(second_line) > 0:
-        action = second_line[0]
+        search_command = second_line[0]
         arg = second_line[2:] if len(second_line) > 2 else None
 
-    while not verify_action(action, arg):
+    while not verify_search_command(search_command, arg):
         print("ERROR")
         second_line = input().strip()
 
         if len(second_line) > 0:
-            action = second_line[0]
+            search_command = second_line[0]
             arg = second_line[2:] if len(second_line) > 2 else None
         else:
-            action = None
+            search_command = None
 
-    handle_search(root, action, arg)
-    print(search_result)
+    
+    search_result = handle_search(root, search_command, arg)
+
+    action = input()
+    while not verify_action(action):
+        print("ERROR")
+        action = input()
+
+    handle_actions(action, search_result)
+        
 
 if __name__ == '__main__':
     main()
